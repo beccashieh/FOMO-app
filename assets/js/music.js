dc8cb416314c9f76aee2a5cb97ab4a64cf682900
Steve Travis <stevetravis11@gmail.com>
2019-04-11 19:23:53 -0400

    Working to add response data to html

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,48 +1,46 @@
 //Global variables
 var apiKey = "10f8d0af1f97ab76b64e7be5940dcbd0";
-var queryUrl =
+var topArtistQueryUrl =
   "https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" +
+  apiKey +
+  "&limit=10&format=json";
+var topSongQueryUrl =
+  "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" +
   apiKey +
   "&limit=10&format=json";
 //API call for Top 10 Artists
 $(document).on("click", "#top-artists", function() {
-  // Storing our giphy API URL
-
-  var queryUrl =
-    "https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=" +
-    apiKey +
-    "&limit=10&format=json";
   // Perfoming an AJAX GET request to our queryURL
   $.ajax({
-    url: queryUrl,
+    url: topArtistQueryUrl,
     method: "GET"
   }).then(function(response) {
     // Storing an array of artist names
-    console.log(response);
+    // console.log(response);
     var artistArray = response.artists.artist;
     // console.log(artistArray);
     // Looping over every result item
     for (var i = 0; i < artistArray.length; i++) {
       // Storing artist names
+      var artistDiv = $("<div>");
       var artistName = artistArray[i].name;
       var artistImage = artistArray[i].image[2]["#text"];
-      console.log(artistName);
-      console.log(artistImage);
+      var p = $("<p class='flow-text textInfo'>").text + artistName;
+      var artistImg = $("<img>");
+      artistImg.attr("src", artistImage);
+      artistDiv.append(p);
+      artistDiv.append(artistImg);
+      $("#top-10-artists").prepend(artistImage);
+      $("#top-10-artists").prepend(artistDiv);
     }
   });
 });
 
 //API call for Top 10 Songs
 $("#top-songs").click(function() {
-  // Storing our giphy API URL
-  var apiKey = "10f8d0af1f97ab76b64e7be5940dcbd0";
-  var queryUrl =
-    "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" +
-    apiKey +
-    "&limit=10&format=json";
   // Perfoming an AJAX GET request to our queryURL
   $.ajax({
-    url: queryUrl,
+    url: topSongQueryUrl,
     method: "GET"
   }).then(function(response) {
     // Storing an array of artist names
