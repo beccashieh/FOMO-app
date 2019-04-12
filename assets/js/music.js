a7e71f2b39ed02e21b580d520b96c56a9a84aa7a
Steve Travis <stevetravis11@gmail.com>
2019-04-12 14:58:13 -0400

    Song cards working. Centered buttons.

--- assets/js/music.js
+++ assets/js/music.js
@@ -22,43 +22,31 @@ $(document).on("click", "#top-artists-button", function() {
     // Looping over every result item
     for (var i = 0; i < artistArray.length; i++) {
       var newCard = $("<div>");
-      newCard.attr("class", "card news-item");
+      newCard.attr("class", "card artist-item");
       // Storing artist names
       var artistName = artistArray[i].name;
       var artistImage = artistArray[i].image[2]["#text"];
+      var artistUrl = artistArray[i].url;
+      var artistRank = i + 1;
       var cardPicHolder = $("<div>");
-      cardPicHolder.attr("class", "card-image");
+      cardPicHolder.attr("class", "card-image hoverable artist-card");
       var cardPic = $("<img>");
       cardPic.attr("src", artistImage);
       cardPic.attr("class", "card-image");
       cardPic.appendTo(cardPicHolder);
       cardPicHolder.appendTo(newCard);
-
       var cardBody = $("<div>");
-      cardBody.attr("class", "card-content");
-      cardBody.append("<h5>" + artistName + "</h5>");
-      cardBody.append("<p>" + artistName + "</p>");
+      cardBody.attr("class", "");
+      cardBody.append("<h5> # " + artistRank + "</h5>");
+      cardBody.append("<h4>" + artistName + "</h4>");
       cardBody.appendTo(newCard);
-
       var cardLinks = $("<div>");
-      cardLinks.attr("class", "");
-      var artistLink = $("<a>");
-      artistLink.attr("src", "#");
-      artistLink.text("Find out more!");
-      artistLink.appendTo(newCard);
-
+      cardLinks.attr("class", "card-action");
       var cardColumn = $("<div>");
       cardColumn.attr("class", "col s3");
       newCard.appendTo(cardColumn);
-      cardColumn.appendTo($("#topartists"));
+      cardColumn.prependTo($("#output"));
     }
-    // var p = $("<p>").text(artistName);
-    // var artistImg = $("<img>");
-    // artistImg.attr("src", artistImage);
-    // artistImg.addClass("img col s3");
-    // artistDiv.append(p);
-    // artistDiv.append(artistImg);
-    // $("#topartists").prepend(artistDiv);
   });
 });
 
@@ -70,22 +58,33 @@ $("#top-songs-button").click(function() {
     method: "GET"
   }).then(function(response) {
     // Storing an array of artist names
-    // console.log(response);
     var tracksArray = response.tracks.track;
-    // console.log(tracksArray);
-    // Looping over every result item
     for (var i = 0; i < tracksArray.length; i++) {
+      var newCard = $("<div>");
+      newCard.attr("class", "card song-item");
       // Storing artist names
-      var trackDiv = $("<div>");
       var trackName = tracksArray[i].name;
       var trackImage = tracksArray[i].image[2]["#text"];
-      var p = $("<p>").text(trackName);
-      var trackImg = $("<img>");
-      trackImg.attr("src", trackImage);
-      trackImg.addClass("img col s3");
-      trackDiv.append(p);
-      trackDiv.append(trackImg);
-      $("#top-songs").prepend(trackDiv);
+      var tracktUrl = tracksArray[i].url;
+      var trackRank = i + 1;
+      var cardPicHolder = $("<div>");
+      cardPicHolder.attr("class", "card-image hoverable artist-card");
+      var cardPic = $("<img>");
+      cardPic.attr("src", trackImage);
+      cardPic.attr("class", "card-image");
+      cardPic.appendTo(cardPicHolder);
+      cardPicHolder.appendTo(newCard);
+      var cardBody = $("<div>");
+      cardBody.attr("class", "");
+      cardBody.append("<h5> # " + trackRank + "</h5>");
+      cardBody.append("<h4>" + trackName + "</h4>");
+      cardBody.appendTo(newCard);
+      var cardLinks = $("<div>");
+      cardLinks.attr("class", "card-action");
+      var cardColumn = $("<div>");
+      cardColumn.attr("class", "col s3");
+      newCard.appendTo(cardColumn);
+      cardColumn.prependTo($("#output"));
     }
   });
 });
