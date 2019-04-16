1e25dd8a8c647ebb61265ae15ed3010e2fc85b95
erharmon <emerson.harmon@gmail.com>
2019-04-16 17:56:23 -0400

    updated style and axios, error handling

--- assets/js/news.js
+++ assets/js/news.js
@@ -4,14 +4,16 @@ var newsInput = $('#news-query-input').val();
 var searchURL = 'https://newsapi.org/v2/everything?' +
 'q=' + newsInput +
 '&apiKey=4d40ce544309489b9dd043dfac1e6abf'
-    $.ajax({
-        url: searchURL,
-        method: 'GET'
-    }).then(function (result) {
-        
+axios.get(searchURL).then(function (result) {
+        event.preventDefault()
         console.log(searchURL)
         console.log(result)
-        for (var i = 1; i < result.articles.length; i++) {
+        if (result.data.articles.length === 0) {
+            blankDiv = $('<div>')
+            blankDiv.append("<h3> Sorry, but your search didn't turn up any results. </h3>");
+            blankDiv.appendTo($('#news-item-box'))
+        }
+        for (var i = 1; i < result.data.articles.length; i++) {
             
             var newCard = $('<div>')
             newCard.attr('class', 'card news-item')
@@ -19,26 +21,27 @@ var searchURL = 'https://newsapi.org/v2/everything?' +
             var cardPicHolder = $('<div>')
             cardPicHolder.attr('class', 'card-image')
             var cardPic = $('<img>')
-            cardPic.attr('src', result.articles[i].urlToImage)
+            cardPic.attr('src', result.data.articles[i].urlToImage)
             cardPic.attr('class' , 'card-image')
             cardPic.appendTo(cardPicHolder)
             cardPicHolder.appendTo(newCard)
 
             var cardBody = $('<div>')
             cardBody.attr('class', 'card-content')
-            if (result.articles[i].urlToImage === null) {
+            if (result.data.articles[i].urlToImage === null) {
                 console.log('working')
-                cardBody.append('<h3>' + result.articles[i].title + '</h3>')
-                cardBody.append('<h5>' + result.articles[i].source.name + '</6>')
+                cardBody.append('<h3>' + result.data.articles[i].title + '</h3>')
+                cardBody.append('<h5>' + result.data.articles[i].source.name + '</6>')
             } else {
-            cardBody.append('<h5>' + result.articles[i].title + '</h5>')
-            cardBody.append('<p>' + result.articles[i].source.name + '</p>') }
+            cardBody.append('<h5>' + result.data.articles[i].title + '</h5>')
+            cardBody.append('<p>' + result.data.articles[i].source.name + '</p>') }
             cardBody.appendTo(newCard)
 
             var cardLinks = $('<div>')
             cardLinks.attr('class' , 'card-action')
             var newsLink = $('<a>')
-            newsLink.attr('href', result.articles[i].url)
+            newsLink.attr('href', result.data.articles[i].url)
+            newsLink.attr('target', 'blank');
             newsLink.text('Check it out!')
             newsLink.appendTo(cardLinks)
             cardLinks.appendTo(newCard)
@@ -53,7 +56,7 @@ var searchURL = 'https://newsapi.org/v2/everything?' +
                 var newRow = $('<div>')
                 newRow.attr('class', 'row')
                 newRow.attr('id', 'news-item-box')
-                newRow.appendTo($('#content'))
+                newRow.appendTo($('#results'))
             }
         }
             var newCard = $('<div>')
@@ -62,25 +65,25 @@ var searchURL = 'https://newsapi.org/v2/everything?' +
             var cardPicHolder = $('<div>')
             cardPicHolder.attr('class', 'card-image')
             var cardPic = $('<img>')
-            cardPic.attr('src', result.articles[0].urlToImage)
+            cardPic.attr('src', result.data.articles[0].urlToImage)
             cardPic.attr('class' , 'card-image')
             cardPic.appendTo(cardPicHolder)
             cardPicHolder.appendTo(newCard)
 
             var cardBody = $('<div>')
             cardBody.attr('class', 'card-content')
-            if (result.articles[0].urlToImage === null) {
-                cardBody.append('<h3>' + result.articles[0].title + '</h3>')
-                cardBody.append('<h6>' + result.articles[0].source.name + '</6>')
+            if (result.data.articles[0].urlToImage === null) {
+                cardBody.append('<h3>' + result.data.articles[0].title + '</h3>')
+                cardBody.append('<h6>' + result.data.articles[0].source.name + '</6>')
             } else {
-            cardBody.append('<h5>' + result.articles[0].title + '</h5>')
-            cardBody.append('<p>' + result.articles[0].source.name + '</p>') }
+            cardBody.append('<h5>' + result.data.articles[0].title + '</h5>')
+            cardBody.append('<p>' + result.data.articles[0].source.name + '</p>') }
             cardBody.appendTo(newCard)
 
             var cardLinks = $('<div>')
             cardLinks.attr('class' , 'card-action')
             var newsLink = $('<a>')
-            newsLink.attr('href', result.articles[0].url)
+            newsLink.attr('href', result.data.articles[0].url)
             newsLink.text('Check it out!')
             newsLink.appendTo(cardLinks)
             cardLinks.appendTo(newCard)
@@ -89,6 +92,8 @@ var searchURL = 'https://newsapi.org/v2/everything?' +
             cardColumn.attr('class' , 'col s3')
             newCard.appendTo(cardColumn);
             cardColumn.appendTo($('#news-item-box'));
+
+            $('#results').slideDown(1000)
 
     })
     
