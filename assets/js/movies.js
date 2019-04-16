cc7633ec2a9df50f1498f8cf8607640d808cfc28
Kevin Schroeder <schroederkevin94@gmail.com>
2019-04-16 18:47:58 -0400

    updated movie page with css and login

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -22,89 +22,97 @@
 // var hasCustomGenre = false;
 // var startDateInput = "";
 // var endDateInput = "";
-// var genres = [{
-//         "id": 28,
-//         "name": "Action"
-//     },
-//     {
-//         "id": 12,
-//         "name": "Adventure"
-//     },
-//     {
-//         "id": 16,
-//         "name": "Animation"
-//     },
-//     {
-//         "id": 35,
-//         "name": "Comedy"
-//     },
-//     {
-//         "id": 80,
-//         "name": "Crime"
-//     },
-//     {
-//         "id": 99,
-//         "name": "Documentary"
-//     },
-//     {
-//         "id": 18,
-//         "name": "Drama"
-//     },
-//     {
-//         "id": 10751,
-//         "name": "Family"
-//     },
-//     {
-//         "id": 14,
-//         "name": "Fantasy"
-//     },
-//     {
-//         "id": 36,
-//         "name": "History"
-//     },
-//     {
-//         "id": 27,
-//         "name": "Horror"
-//     },
-//     {
-//         "id": 10402,
-//         "name": "Music"
-//     },
-//     {
-//         "id": 9648,
-//         "name": "Mystery"
-//     },
-//     {
-//         "id": 10749,
-//         "name": "Romance"
-//     },
-//     {
-//         "id": 878,
-//         "name": "Science Fiction"
-//     },
-//     {
-//         "id": 10770,
-//         "name": "TV Movie"
-//     },
-//     {
-//         "id": 53,
-//         "name": "Thriller"
-//     },
-//     {
-//         "id": 10752,
-//         "name": "War"
-//     },
-//     {
-//         "id": 37,
-//         "name": "Western"
-//     }
-// ];
+
 
 let movieQueryURL = '';
 let tvQueryURL = '';
 let recTVQueryURL = '';
 let genre = '';
 let genreID = 0;
+let userID = "";
+let userLoggedIn = false;
+let startDateInput = '';
+let endDateInput = '';
+let startDateDisplay = '';
+let endDateDisplay = '';
+let hasDateInput = false;
+let genreOptions = [{
+        "id": 28,
+        "name": "Action"
+    },
+    {
+        "id": 12,
+        "name": "Adventure"
+    },
+    {
+        "id": 16,
+        "name": "Animation"
+    },
+    {
+        "id": 35,
+        "name": "Comedy"
+    },
+    {
+        "id": 80,
+        "name": "Crime"
+    },
+    {
+        "id": 99,
+        "name": "Documentary"
+    },
+    {
+        "id": 18,
+        "name": "Drama"
+    },
+    {
+        "id": 10751,
+        "name": "Family"
+    },
+    {
+        "id": 14,
+        "name": "Fantasy"
+    },
+    {
+        "id": 36,
+        "name": "History"
+    },
+    {
+        "id": 27,
+        "name": "Horror"
+    },
+    {
+        "id": 10402,
+        "name": "Music"
+    },
+    {
+        "id": 9648,
+        "name": "Mystery"
+    },
+    {
+        "id": 10749,
+        "name": "Romance"
+    },
+    {
+        "id": 878,
+        "name": "Science Fiction"
+    },
+    {
+        "id": 10770,
+        "name": "TV Movie"
+    },
+    {
+        "id": 53,
+        "name": "Thriller"
+    },
+    {
+        "id": 10752,
+        "name": "War"
+    },
+    {
+        "id": 37,
+        "name": "Western"
+    }
+];
 
 $(document).ready(function () {
     $('.modal').modal();
@@ -114,26 +122,52 @@ $(document).ready(function () {
     $('.card').modal();
 });
 
-// get data
-db.collection('cards').get().then(snapshot => {
-    createCards(snapshot.docs);
-});
+
 
 //listen for auth status changes
 auth.onAuthStateChanged(user => {
     // CHANGE IF THE USER IS LOGGED IN
     if (user) {
-        console.log(user);
+        // runs every time something changes in the database
+        db.collection('users').doc(user.uid).onSnapshot(doc => {
+            $("#movie-row").empty();
+            $("#tv-row").empty();
+            $("#suggestTV-row").empty();
+
+            userLoggedIn = true;
+            userID = doc.id;
+            genre = doc.data().genre;
+            genre = genre.charAt(0).toUpperCase() + genre.slice(1);
+            console.log(genre);
+            console.log(userID);
+
+            generateGenreID();
+            generateCustomURLs();
+            getMovies();
+            getTV();
+            getSuggestedTV();
+
+        });
     } else {
+        $("#movie-row").empty();
+        $("#tv-row").empty();
+        $("#suggestTV-row").empty();
         console.log('user logged out');
+        genre = '';
+        generateGenreID();
+        generateGenericURLs();
+        getMovies();
+        getTV();
+        $("#suggestTV-header").css("display", "none");
     }
 });
 
+// edit favorite genre
 const createForm = document.querySelector('#create-form');
 createForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    db.collection('cards').add({
+    db.collection('users').doc(userID).update({
         genre: createForm['genre'].value
     }).then(() => {
         // close modal and clear fields
@@ -141,7 +175,6 @@ createForm.addEventListener('submit', (e) => {
         M.Modal.getInstance(modal).close();
         createForm.reset();
     });
-
 });
 
 // signup
@@ -155,6 +188,10 @@ signupForm.addEventListener('submit', (e) => {
 
     // signup the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            genre: signupForm['favorite-genre'].value
+        });
+    }).then(() => {
         // close modal and clear fields
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
@@ -186,25 +223,246 @@ loginForm.addEventListener('submit', (e) => {
     });
 });
 
-const cardList = document.querySelector('.card-row');
 
-const createCards = (data) => {
-    //Axios request
-    let html = '';
-    data.forEach(doc => {
-        const card = doc.data();
-        genre = card.genre;
-    });
-    generateURLs();
+
+// const createCards = (data) => {
+
+//     data.forEach(doc => {
+//         const card = doc.data();
+//         genre = card.genre;
+//     });
+
+//     generateGenreID();
+//     generateURLs();
+//     getMovies();
+// }
+
+const generateGenreID = () => {
+    for (i = 0; i < genreOptions.length; i++) {
+        if (genre === genreOptions[i].name) {
+            genreID = genreOptions[i].id;
+        }
+    }
 }
 
-const generateURLs = () => {
-    movieQueryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c472f72e061f4083caae81e6e937276a&sort_by=popularity.desc&with_genres=" + genreID + "";
+const generateGenericURLs = () => {
+    movieQueryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c472f72e061f4083caae81e6e937276a&sort_by=popularity.desc";
     tvQueryURL = "https://api.themoviedb.org/3/discover/tv?api_key=c472f72e061f4083caae81e6e937276a&sort_by=popularity.desc";
-    suggestTVQueryURL = "https://api.themoviedb.org/3/discover/tv?api_key=c472f72e061f4083caae81e6e937276a&language=en&page=1&sort_by=popularity.desc&with_genres=" + genreID + "";
+    suggestTVQueryURL = "https://api.themoviedb.org/3/discover/tv?api_key=c472f72e061f4083caae81e6e937276a&language=en&page=1&sort_by=popularity.desc";
+}
+const generateCustomURLs = () => {
+    console.log(hasDateInput)
+
+    if (hasDateInput === false) {
+        movieQueryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c472f72e061f4083caae81e6e937276a&sort_by=popularity.desc&with_genres=" + genreID + "";
+        tvQueryURL = "https://api.themoviedb.org/3/discover/tv?api_key=c472f72e061f4083caae81e6e937276a&sort_by=popularity.desc";
+        suggestTVQueryURL = "https://api.themoviedb.org/3/discover/tv?api_key=c472f72e061f4083caae81e6e937276a&language=en&page=1&sort_by=popularity.desc&with_genres=" + genreID + "";
+    } else if (hasDateInput === true){
+        movieQueryURL = "https://api.themoviedb.org/3/discover/movie?api_key=c472f72e061f4083caae81e6e937276a&with_genres=" + genreID + "&primary_release_date.gte=" + startDateInput + "&primary_release_date.lte=" + endDateInput + "&sort_by=popularity.desc";
+        tvQueryURL = "https://api.themoviedb.org/3/discover/tv?api_key=c472f72e061f4083caae81e6e937276a&sort_by=popularity.desc";
+        suggestTVQueryURL = "https://api.themoviedb.org/3/discover/tv?api_key=c472f72e061f4083caae81e6e937276a&language=en&page=1&sort_by=popularity.desc&with_genres=" + genreID + "";
+    }
 }
 
-// const cards = document.querySelector('.content-holder');
+const movieList = document.querySelector('#movie-row');
+// Axios request
+function getMovies() {
+    axios.get(movieQueryURL).then(function (response) {
+        console.log(response)
+
+        // generate section header
+        if (userLoggedIn === true && hasDateInput === false) {
+            $("#movie-header").css("display", "block").text(`Top ${genre} Movies Available Today`);
+        } else if (userLoggedIn === true && hasDateInput === true) {
+            $("#movie-header").css("display", "block").text(`Top ${genre} Movies from ${startDateDisplay} to ${endDateDisplay}`);
+        } else {
+            $("#movie-header").css("display", "block").text(`Most Popular Movies Available Today`);
+        }
+
+        var html = '';
+        let cardCount = 0;
+        for (let j = 0; cardCount < 6; j++) {
+            if (response.data.results[j].original_language === 'en') {
+                var name = response.data.results[j].original_title;
+                var imageURL = response.data.results[j].poster_path;
+                var overview = response.data.results[j].overview;
+
+                let cardItem = `
+                <div class="col l2 col m4 col s4">
+                    <div class = "card">
+                        <div class = "card-image waves-effect waves-block waves-light">
+                            <img class = "activator" src = "https://image.tmdb.org/t/p/w200${imageURL}">
+                        </div> 
+
+                        <div class = "card-content">
+                            <span class = "card-title activator grey-text text-darken-4"> ${name} 
+                                <i class = "material-icons right"> more_vert </i>
+                            </span>
+                        </div> 
+                        
+                        <div class = "card-reveal">
+                            <span class = "card-title grey-text text-darken-4"> 
+                                <i class = "material-icons right"> close </i>
+                                ${name}
+                            </span>
+                            <p>${overview}</p> 
+                        </div> 
+                    </div>
+                </div>
+                `;
+                html = html + cardItem;
+                cardCount++;
+            }
+        }
+        movieList.innerHTML = html
+    });
+
+};
+
+const suggestTVList = document.querySelector('#tv-row');
+
+function getTV() {
+    axios.get(tvQueryURL).then(function (response) {
+        console.log(response)
+
+        // generate section header
+        $("#tv-header").css("display", "block").text(`Most Popular TV Shows Available Today`);
+
+
+        var html = '';
+        let cardCount = 0;
+        for (let j = 0; cardCount < 6; j++) {
+            if (response.data.results[j].original_language === 'en') {
+                var name = response.data.results[j].original_name;
+                var imageURL = response.data.results[j].poster_path;
+                var overview = response.data.results[j].overview;
+
+                let cardItem = `
+                <div class="col l2 col m4 col s4">
+                    <div class = "card">
+                        <div class = "card-image waves-effect waves-block waves-light">
+                            <img class = "activator" src = "https://image.tmdb.org/t/p/w200${imageURL}">
+                        </div> 
+
+                        <div class = "card-content">
+                            <span class = "card-title activator grey-text text-darken-4"> ${name} 
+                                <i class = "material-icons right"> more_vert </i>
+                            </span>
+                        </div> 
+                        
+                        <div class = "card-reveal">
+                            <span class = "card-title grey-text text-darken-4"> 
+                                <i class = "material-icons right"> close </i>
+                                ${name}
+                            </span>
+                            <p>${overview}</p> 
+                        </div> 
+                    </div>
+                </div>
+                `;
+                html = html + cardItem;
+                cardCount++;
+            }
+        }
+        suggestTVList.innerHTML = html
+    });
+
+};
+
+const tvList = document.querySelector('#suggestTV-row');
+
+function getSuggestedTV() {
+
+    axios.get(suggestTVQueryURL).then(function (response) {
+
+        console.log(response)
+
+        // generate section header
+        $("#suggestTV-header").css("display", "block").text(`Suggested TV Shows Based on Your Movie Preferences`);
+
+        var html = '';
+        let cardCount = 0;
+        for (let j = 0; cardCount < 6; j++) {
+            if (response.data.results[j].original_language === 'en') {
+                var name = response.data.results[j].original_name;
+                var imageURL = response.data.results[j].poster_path;
+                var overview = response.data.results[j].overview;
+
+                let cardItem = `
+                <div class="col l2 col m4 col s4">
+                    <div class = "card">
+                        <div class = "card-image waves-effect waves-block waves-light">
+                            <img class = "activator" src = "https://image.tmdb.org/t/p/w200${imageURL}">
+                        </div> 
+
+                        <div class = "card-content">
+                            <span class = "card-title activator grey-text text-darken-4"> ${name} 
+                                <i class = "material-icons right"> more_vert </i>
+                            </span>
+                        </div> 
+                        
+                        <div class = "card-reveal">
+                            <span class = "card-title grey-text text-darken-4"> 
+                                <i class = "material-icons right"> close </i>
+                                ${name}
+                            </span>
+                            <p>${overview}</p> 
+                        </div> 
+                    </div>
+                </div>
+                `;
+                html = html + cardItem;
+                cardCount++;
+            }
+        }
+        tvList.innerHTML = html
+    });
+
+};
+
+$('#date-button').click(function () {
+    //Prevent default
+    event.preventDefault();
+    $("#movie-row").empty();
+    $("#tv-row").empty();
+    $("#suggest-tv-row").empty();
+
+    hasDateInput = true;
+
+    //Capture date in the input box
+    startDateInput = $("#startDate").val();
+    let startDateObj = new Date(startDateInput);
+    startDateObj.setHours(startDateObj.getHours() + 7);
+
+
+    let startMM = startDateObj.getMonth() + 1;
+    let startDD = startDateObj.getDate();
+    let startYYYY = startDateObj.getFullYear();
+    startDateDisplay = startMM + "/" + startDD + "/" + startYYYY
+
+    endDateInput = $("#endDate").val();
+    let endDateObj = new Date(endDateInput);
+    endDateObj.setHours(endDateObj.getHours() + 7);
+
+    let endMM = endDateObj.getMonth() + 1;
+    let endDD = endDateObj.getDate();
+    let endYYYY = endDateObj.getFullYear();
+    endDateDisplay = endMM + "/" + endDD + "/" + endYYYY
+
+    //generateGenreID();
+    generateCustomURLs();
+    getMovies();
+    getTV();
+    getSuggestedTV();
+
+});
+
+
+
+
+
+
+
 
 // //setup cards
 // function setupCards (data) {
