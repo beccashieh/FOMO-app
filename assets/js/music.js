7e3ed63129bf09cfb05d99ef763e0b89c4034211
Steve Travis <stevetravis11@gmail.com>
2019-04-18 20:49:21 -0400

    Firebase working, fixed autocomplete dropdown.

--- assets/js/music.js
+++ assets/js/music.js
@@ -8,6 +8,8 @@ var topSongQueryUrl =
   "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" +
   apiKey +
   "&limit=10&format=json";
+var userLoggedIn = false;
+var favArtist = "";
 
 //Autocomplete function for country list
 $(document).ready(function() {
@@ -23,7 +25,7 @@ $(document).ready(function() {
         }
         $("input.autocomplete").autocomplete({
           data: dataCountry,
-          limit: 5 // The max amount of results that can be shown at once. Default: Infinity.
+          limit: 2 // The max amount of results that can be shown at once. Default: Infinity.
         });
       }
     });
@@ -42,18 +44,27 @@ $(document).ready(function() {
   $(".sidenav").sidenav();
 });
 
-//listen for auth status changes
-// auth.onAuthStateChanged(user => {
-//   // CHANGE IF THE USER IS LOGGED IN
-//   if (user) {
-//     // runs every time something changes in the database
-//     db.collection("users")
-//       .doc(user.uid)
-//       .onSnapshot(doc => {});
-//     //IF THE USER IS NOT LOGGED IN DO...
-//   } else {
-//   }
-// });
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+  // CHANGE IF THE USER IS LOGGED IN
+  if (user) {
+    // runs every time something changes in the database
+    db.collection("users")
+      .doc(user.uid)
+      .onSnapshot(doc => {
+        $("#output").empty();
+
+        userLoggedIn = true;
+        userID = doc.id;
+        favArtist = doc.data().artist;
+        favoriteArtist();
+      });
+    //IF THE USER IS NOT LOGGED IN DO...
+  } else {
+    $("#output").empty();
+    favArtist = "";
+  }
+});
 
 // signup
 const signupForm = document.querySelector("#signup-form");
@@ -121,7 +132,7 @@ $(document).on("click", "#top-artists-button", function() {
     // Looping over every result item
     for (var i = artistArray.length - 1; i >= 0; i--) {
       var newCard = $("<div>");
-      newCard.attr("class", "card med artist-item");
+      newCard.attr("class", "card small artist-item");
       // Storing artist names
       var artistName = artistArray[i].name;
       // Storing URL for artist image
@@ -153,7 +164,7 @@ $(document).on("click", "#top-artists-button", function() {
       var cardLinks = $("<div>");
       cardLinks.attr("class", "card-action");
       var cardColumn = $("<div>");
-      cardColumn.attr("class", "col s3");
+      cardColumn.attr("class", "col s12 m6 l2");
       newCard.prependTo(cardColumn);
       cardColumn.prependTo($("#output"));
     }
@@ -170,7 +181,7 @@ $("#top-songs-button").click(function() {
     var tracksArray = response.data.tracks.track;
     for (var i = tracksArray.length - 1; i >= 0; i--) {
       var newCard = $("<div>");
-      newCard.attr("class", "card medium song-item");
+      newCard.attr("class", "card small song-item");
       // Storing artist names
       var trackName = tracksArray[i].name;
       var trackImage = tracksArray[i].image[3]["#text"];
@@ -197,7 +208,7 @@ $("#top-songs-button").click(function() {
       var cardLinks = $("<div>");
       cardLinks.attr("class", "card-action");
       var cardColumn = $("<div>");
-      cardColumn.attr("class", "col s3");
+      cardColumn.attr("class", "col s12 m6 l2");
       newCard.appendTo(cardColumn);
       cardColumn.prependTo($("#output"));
     }
@@ -226,7 +237,7 @@ $("#top-artist-country-button").click(function() {
     // console.log(response);
     for (var i = artistArray.length - 1; i >= 0; i--) {
       var newCard = $("<div>");
-      newCard.attr("class", "card medium artist-item");
+      newCard.attr("class", "card small artist-item");
       // Storing artist names
       var artistName = artistArray[i].name;
       // Storing URL for artist image
@@ -258,7 +269,7 @@ $("#top-artist-country-button").click(function() {
       var cardLinks = $("<div>");
       cardLinks.attr("class", "card-action");
       var cardColumn = $("<div>");
-      cardColumn.attr("class", "col s3");
+      cardColumn.attr("class", "col s12 m6 l2");
       newCard.appendTo(cardColumn);
       cardColumn.prependTo($("#output"));
     }
@@ -287,7 +298,7 @@ $("#top-songs-country-button").click(function() {
     // console.log(response);
     for (var i = tracksArray.length - 1; i >= 0; i--) {
       var newCard = $("<div>");
-      newCard.attr("class", "card medium song-item");
+      newCard.attr("class", "card small song-item");
       // Storing artist names
       var trackName = tracksArray[i].name;
       var trackImage = tracksArray[i].image[3]["#text"];
@@ -314,9 +325,72 @@ $("#top-songs-country-button").click(function() {
       var cardLinks = $("<div>");
       cardLinks.attr("class", "card-action");
       var cardColumn = $("<div>");
-      cardColumn.attr("class", "col s3");
+      cardColumn.attr("class", "col s12 m6 l2");
       newCard.appendTo(cardColumn);
       cardColumn.prependTo($("#output"));
     }
   });
 });
+
+function favoriteArtist() {
+  // Find similar artists to favorite artist
+  var similarArtistUrl =
+    "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" +
+    favArtist +
+    "&api_key=" +
+    apiKey +
+    "&limit=6&format=json";
+  //Clear output section
+  $("#output").empty();
+  // Perfoming an AJAX GET request to our queryURL
+  axios.get(similarArtistUrl).then(function(response) {
+    console.log(response);
+    // Storing an array of artist names
+    var artistArray = response.data.similarartists.artist;
+    // console.log(artistArray);
+    // Looping over every result item
+    // console.log(response);
+    for (var i = artistArray.length - 1; i >= 0; i--) {
+      var newCard = $("<div>");
+      newCard.attr("class", "card small artist-item");
+      // Storing artist names
+      var artistName = artistArray[i].name;
+      // Storing URL for artist image
+      var artistImage = artistArray[i].image[3]["#text"];
+      // Storing URL for arist last.fm page
+      var artistUrl = artistArray[i].url;
+      // Creating artist rank variable
+      // var artistRank = i + 1;
+      // Begin building cards
+      var cardPicHolder = $("<div>");
+      cardPicHolder.attr(
+        "class",
+        "responsive-img card-image hoverable artist-card"
+      );
+      var cardUrl = $("<a>");
+      cardUrl.attr("href", artistUrl);
+      cardUrl.prependTo(cardPicHolder);
+      var cardPic = $("<img>");
+      cardPic.attr("src", artistImage);
+      cardPic.attr("class", "card-image");
+
+      cardPic.appendTo(cardUrl);
+      cardPicHolder.appendTo(newCard);
+      var cardBody = $("<div>");
+      cardBody.attr("class", "");
+      // cardBody.append("<h5> # " + artistRank + "</h5>");
+      cardBody.append("<h5>" + artistName + "</h5>");
+      cardBody.appendTo(newCard);
+      var cardLinks = $("<div>");
+      cardLinks.attr("class", "card-action");
+      var cardColumn = $("<div>");
+      cardColumn.attr("class", "col s12 m6 l2");
+      newCard.appendTo(cardColumn);
+      cardColumn.prependTo($("#output"));
+    }
+    // Adds title above output
+    var artistTitle = $("<h3>");
+    artistTitle.text("Artists similar to " + favArtist);
+    artistTitle.prependTo("#output");
+  });
+}
